ed74849f200ca3a8e193ef18be9f242f93516c1c
RCHD <richard.alex02@gmail.com>
2021-03-25 10:29:59 +0100

    add data class ghibliFilm add permission for internet add layout for item inside list of film add lib for recycler view and retrofit

---
+++ app/src/main/java/com/example/testtechniqueidean/ghibliFilm.kt
@@ -0,0 +1,3 @@
+package com.example.testtechniqueidean
+
+data class ghibliFilm(val title: String,val release_date:Int,val ImageFilm:String,val producer:String , val director:String, val description:String, var liked : Boolean)
